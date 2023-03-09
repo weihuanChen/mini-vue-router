07076a0d18ac4a3032ea4ba1d90eb9f05c12e32a
chenqi <13265255252@163.com>
2023-03-09 14:00:27 +0800

    feat:实现对url的监听

--- src/my-router/router.js
+++ src/my-router/router.js
@@ -1,5 +1,6 @@
 import RouterLink from "./RouterLink.js";
 import RouterView from "./RouterView.js";
+import { ref } from "vue";
 /**
  *
  * @param {*} options 接收可选配置生成新的router实例
@@ -7,6 +8,8 @@ import RouterView from "./RouterView.js";
  */
 export function createRouter(options) {
   const router = {
+    options, //保存配置项
+    current: ref(window.location.hash.slice(1) || "/"), //把hash井号前的地址切下来做响应式
     //安装方法
     //由于要在全局上挂$router以供用户使用，因此要接收一个app实例
     install(app) {
@@ -21,5 +24,10 @@ export function createRouter(options) {
       app.component("RouterView", RouterView);
     },
   };
+  //监听事件
+  window.addEventListener("hashchange", () => {
+    router.current.value = window.location.hash.slice(1);
+    console.log(router.current.value);
+  });
   return router;
 }
